68709d686742bc73469262f5f5e4040c08981cba
Sylvexia <cuberobin@gmail.com>
2024-09-15 22:12:19 +0800

    temp push

--- src/TestPosit.cpp
+++ src/TestPosit.cpp
@@ -1,21 +1,32 @@
-
-// #include "src/Dialect/Posit/PositOps.hpp"
-// #include "mlir/IR/Dialect.h"
-// #include "mlir/IR/Builders.h"
-// #include "mlir/IR/MLIRContext.h"
-// #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
+#include "src/Dialect/Posit/PositOps.hpp"
 
 int main() {
-//   mlir::MLIRContext context;
-//   context.loadDialect<mlir::PositDialect>();
-//   auto builder = mlir::OpBuilder(&context);
-//   auto module = mlir::ModuleOp::create(builder.getUnknownLoc());
-//   builder.setInsertionPointToStart(module.getBody());
+  mlir::MLIRContext context;
+  context.loadDialect<mlir::PositDialect>();
+  auto builder = mlir::OpBuilder(&context);
+  auto module = mlir::ModuleOp::create(builder.getUnknownLoc());
+  builder.setInsertionPointToStart(module.getBody());
 
-//   // auto lhs = builder.create<mlir::PositConstantOp>(builder.getUnknownLoc(), builder.getUI32IntegerAttr(42));
-//   // mlir::Value rhs =
-//   //     builder.create<mlir::arith::ConstantOp>(builder.getUnknownLoc(), builder.getI32IntegerAttr(42));
-//   // auto addOp = builder.create<mlir::PositAddOp>(builder.getUnknownLoc(), lhs, rhs);
-//   module.print(llvm::outs());
+  mlir::Type f64Type = builder.getF64Type();
+  mlir::FloatAttr f64Attr = builder.getF64FloatAttr(3.14);
+  auto lhs = builder.create<mlir::PositConstantOp>(builder.getUnknownLoc(),
+    f64Type, f64Attr);
+
+  // auto lhs = builder.create<mlir::PositConstantOp>(builder.getUnknownLoc(),
+  //     f64Type, f64Value);
+  // auto lhs = builder.create<mlir::PositConstantOp>(builder.getUnknownLoc(),
+  // 0.42); auto rhs =
+  // builder.create<mlir::PositConstantOp>(builder.getUnknownLoc(), 0.69);
+
+  // auto addOp = builder.create<mlir::PositAddOp>(builder.getUnknownLoc(),
+  // 0.38, 0.43);
+  module.print(llvm::outs());
   return 0;
 }
