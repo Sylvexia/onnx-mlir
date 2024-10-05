5c284821f064fed21bc5d904cdaad19f31fd6f08
sylvexia <cuberobin@gmail.com>
2024-10-05 14:52:45 +0800

    currently the value can be changed but the result type is not handled

--- src/Conversion/ArithToPositFunc/ArithToPositFunc.cpp
+++ src/Conversion/ArithToPositFunc/ArithToPositFunc.cpp
@@ -2,6 +2,8 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/MLIRContext.h"
@@ -11,9 +13,21 @@
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "src/Pass/Passes.hpp"
-#include <sys/types.h>
+
+#define DEBUG_TYPE "convert-arith-to-posit-func"
 
 using namespace mlir;
+
+struct FloatToUIntTypeConverter : public mlir::TypeConverter {
+  FloatToUIntTypeConverter() {
+    addConversion([](Type type) -> Type {
+      if (isa<Float32Type>(type)) {
+        return IntegerType::get(type.getContext(), 32, IntegerType::Signless);
+      }
+      return type;
+    });
+  }
+};
 
 struct ConvertArithToPositFuncPass
     : public PassWrapper<ConvertArithToPositFuncPass, OperationPass<ModuleOp>> {
@@ -22,14 +36,28 @@ struct ConvertArithToPositFuncPass
   StringRef getDescription() const override;
 };
 
+bool isUint32Type(Type type) {
+  if (auto intType = dyn_cast<IntegerType>(type)) {
+    return intType.getWidth() == 32 && intType.isSignless();
+  }
+  return false;
+}
+
 void ConvertArithToPositFuncPass::runOnOperation() {
   auto module = getOperation();
   RewritePatternSet patterns(&getContext());
-  mlir::populateArithToPositFuncConversionPatterns(patterns);
+
+  FloatToUIntTypeConverter typeConverter;
+
+  // mlir::populateArithToPositFuncConversionPatterns(patterns);
+  populateConvertArithConstantFloatToUIntPattern(patterns, typeConverter);
 
   ConversionTarget target(getContext());
   target.addLegalDialect<func::FuncDialect>();
-  target.addIllegalDialect<arith::ArithDialect>();
+  // target.addLegalDialect<arith::ArithDialect>();
+  // target.addIllegalDialect<arith::ArithDialect>();
+  target.addDynamicallyLegalOp<arith::ConstantOp>(
+      [&](arith::ConstantOp op) { return isUint32Type(op.getType()); });
   if (failed(applyPartialConversion(module, target, std::move(patterns))))
     signalPassFailure();
 }
@@ -40,6 +68,52 @@ StringRef ConvertArithToPositFuncPass::getArgument() const {
 
 StringRef ConvertArithToPositFuncPass::getDescription() const {
   return "Lower the arith dialect to posit func dialect.";
+}
+
+struct ConvertArithConstantFloatToUIntPattern
+    : public OpConversionPattern<arith::ConstantOp> {
+  using OpConversionPattern<arith::ConstantOp>::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(arith::ConstantOp op,
+      typename arith::ConstantOp::Adaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    // support only float32
+    if (!isa<Float32Type>(op.getType()))
+      return failure();
+
+    // get the float value
+    auto floatAttr = dyn_cast<FloatAttr>(op.getValue());
+    if (!floatAttr)
+      return failure();
+
+    APFloat apFloat = floatAttr.getValue();
+    uint64_t floatBits = apFloat.bitcastToAPInt().getZExtValue();
+    uint32_t uintValue = static_cast<uint32_t>(floatBits >> 1);
+
+    auto UIntType = getTypeConverter()->convertType(op.getType());
+
+    if (!UIntType)
+      return failure();
+
+    // auto UIntAttr = rewriter.getUI32IntegerAttr(uintValue);
+    auto UIntAttr = rewriter.getIntegerAttr(UIntType, uintValue);
+    auto newOp =
+        rewriter.replaceOpWithNewOp<arith::ConstantOp>(op, UIntType, UIntAttr);
+    llvm::errs() << "After replaceOpWithNewOp:\n";
+    if (newOp) {
+      newOp.print(llvm::errs());
+    } else {
+      llvm::errs() << "Operation was not replaced\n";
+    }
+    // llvm::outs() << "Float value: uwu" << uintValue << "\n";
+    return success();
+  }
+};
+
+void mlir::populateConvertArithConstantFloatToUIntPattern(
+    RewritePatternSet &patterns, TypeConverter &typeConverter) {
+  MLIRContext *ctx = patterns.getContext();
+  patterns.add<ConvertArithConstantFloatToUIntPattern>(typeConverter, ctx);
 }
 
 template <typename Op>
@@ -120,7 +194,8 @@ void populatePatternsForOp(RewritePatternSet &patterns, MLIRContext *ctx,
   patterns.add<ArithToPositFuncLowering<OpTy>>(ctx, opString, n_bits, es_val);
 }
 
-void mlir::populateArithToPositFuncConversionPatterns(RewritePatternSet &patterns) {
+void mlir::populateArithToPositFuncConversionPatterns(
+    RewritePatternSet &patterns) {
   MLIRContext *ctx = patterns.getContext();
   populatePatternsForOp<arith::AddFOp>(patterns, ctx, "add", 8, 0);
   populatePatternsForOp<arith::MulFOp>(patterns, ctx, "mul", 16, 1);
--- src/Conversion/ArithToPositFunc/test.mlir
+++ src/Conversion/ArithToPositFunc/test.mlir
@@ -2,3 +2,15 @@ func.func @test_arith(%arg0 : f32, %arg1 : f32) -> f32 {
   %0 = arith.addf %arg0, %arg1 : f32
   return %0 : f32
 }
+
+func.func @test_const(%float1: f32, %float2: f32) {
+  %0 = arith.constant 2.68 : f32
+  %1 = arith.constant 6.9 : f32
+  return
+}
+
+func.func @test_const_return(%float1: f32, %float2: f32) -> (f32, f32) {
+  %0 = arith.constant 2.68 : f32
+  %1 = arith.constant 6.9 : f32
+  return %0, %1 : f32, f32
+}
