3fed95017abace6782046bf4a79b2af9159cf806
sylvexia <cuberobin@gmail.com>
2024-10-05 14:52:45 +0800

    currently the value can be changed but the result type is not handled

--- src/Conversion/ArithToPositFunc/ArithToPositFunc.hpp
+++ src/Conversion/ArithToPositFunc/ArithToPositFunc.hpp
@@ -2,13 +2,16 @@
 #define MATH_TO_LIB_H_
 
 #include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/DialectConversion.h"
 
 namespace mlir {
 template <typename T>
 class OperationPass;
 
+void populateConvertArithConstantFloatToUIntPattern(
+    RewritePatternSet &patterns, TypeConverter &typeConverter);
 void populateArithToPositFuncConversionPatterns(RewritePatternSet &patterns);
 
-}
+} // namespace mlir
 
 #endif
